4b2e7b93f371bd7bb16d4bdd608f7a8e12e5410b
dmalaniouk <dmalaniouk@gmail.com>
2018-03-19 15:24:57 -0400

    initial

---
+++ QueryLoader2-master/demo/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $("body").queryLoader2({
+        barColor: "#000",
+        backgroundColor: "#333333",
+        percentage: true,
+        barHeight: 10,
+        completeAnimation: "grow",
+        minimumTime: 2000
+    });
+});
+
+
